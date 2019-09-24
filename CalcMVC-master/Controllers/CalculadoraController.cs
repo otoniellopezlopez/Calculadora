19a39050e672186b9b8efb47f62b07ece8ae9779
Gabrielagomezrob <54647304+Gabrielagomezrob@users.noreply.github.com>
2019-09-24 16:49:07 -0500

    Add files via upload

---
+++ CalcMVC-master/Controllers/CalculadoraController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Calculadora.Models;
+
+namespace Calculadora.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        public ActionResult Suma()
+        {
+            ViewData["Suma"] = "1+1=2";
+            return View();
+        }
+public ActionResult resta()
+        {
+            ViewData["resta"] = "20-18=2";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CalcMVC-master/README.md
@@ -0,0 +1,2 @@
+# CalcMVC
+Calculadora en MVC
